6e546883407ed34c538d15ce2b37fc257a9f0e57
mark-cs <2660042+mark-cs@users.noreply.github.com>
2026-02-14 23:06:42 +0000

    highlight selected panel

--- src/tui/view.rs
+++ src/tui/view.rs
@@ -1,10 +1,14 @@
 use ratatui::{
     layout::{Constraint, Direction, Layout, Rect},
     style::Style,
-    widgets::{Block, List, ListDirection},
+    widgets::{Block, BorderType, List, ListDirection},
 };
 
-use crate::{task::Task, tui::model::Model, workspace::Workspace};
+use crate::{
+    task::Task,
+    tui::model::{Model, Panel},
+    workspace::Workspace,
+};
 
 pub fn nav_bar(frame: &mut ratatui::Frame<'_>, container: Rect, model: &mut Model) {
     let nav_layout = Layout::default()
@@ -12,29 +16,51 @@ pub fn nav_bar(frame: &mut ratatui::Frame<'_>, container: Rect, model: &mut Mode
         .constraints(vec![Constraint::Percentage(30), Constraint::Percentage(70)])
         .split(container);
     frame.render_stateful_widget(
-        workspace_list(&model.workspaces()),
+        workspace_list(
+            model.workspaces(),
+            model.focussed_panel == Panel::WorkspaceList,
+        ),
         nav_layout[0],
         model.workspace_selection_mut(),
     );
 
     frame.render_widget(
-        task_list(&model.tasks().unwrap_or(Vec::new())),
+        task_list(
+            &model.tasks().unwrap_or_default(),
+            model.focussed_panel == Panel::TaskList,
+        ),
         nav_layout[1],
     );
 }
 
-pub fn workspace_list<'a>(workspaces: &[Workspace]) -> List<'a> {
+pub fn workspace_list<'a>(workspaces: &[Workspace], panel_selected: bool) -> List<'a> {
     List::new(workspaces.iter().map(|w| w.name.to_owned()))
-        .block(Block::bordered().title("Workspaces"))
+        .block(
+            Block::bordered()
+                .border_type(if panel_selected {
+                    BorderType::Double
+                } else {
+                    BorderType::Plain
+                })
+                .title("Workspaces (w)"),
+        )
         .style(Style::new().white())
         .highlight_style(Style::new().italic())
         .highlight_symbol(">")
         .repeat_highlight_symbol(true)
         .direction(ListDirection::TopToBottom)
 }
-pub fn task_list<'a>(tasks: &[Task]) -> List<'a> {
+pub fn task_list<'a>(tasks: &[Task], panel_selected: bool) -> List<'a> {
     List::new(tasks.iter().map(|w| w.title.to_owned()))
-        .block(Block::bordered().title("Tasks"))
+        .block(
+            Block::bordered()
+                .border_type(if panel_selected {
+                    BorderType::Double
+                } else {
+                    BorderType::Plain
+                })
+                .title("Tasks (l)"),
+        )
         .style(Style::new().white())
         .highlight_style(Style::new().italic())
         .highlight_symbol(">")
